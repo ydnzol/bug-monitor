8d50b2ca4ecf3cafd7fad6dfe2b887e56a2b07fb
杨东宁21450 <21450@sangfor.com>
2020-03-21 15:20:53 +0800

    【修改】配置文件修改。

--- config/config.py
+++ config/config.py
@@ -14,26 +14,24 @@ class Config(object):
     """
     扫描器配置类
     """
+    CVE_API = {
+        "username": "langke",
+        "password": "langke,1234"
+    }
 
+    Wechat = {
+        "app_id": "langke",
+        "app_secret": "langke,1234"
+    }
 
-CVE_API = {
-    "username": "langke",
-    "password": "langke,1234"
-}
+    MONGODB_CFG = {
+        'host': '127.0.0.1',
+        'port': 27017,
+        'user': 'root',
+        'passwd': 'langke',
+        'data_db': 'bug-monitor',
+        'table': 'SecurityProblem'
+    }
 
-
-Wechat = {
-    "app_id": "langke",
-    "app_secret": "langke,1234"
-}
-
-MONGODB_CFG = {
-    'host': '127.0.0.1',
-    'port': 27017,
-    'user': 'root',
-    'passwd': 'langke',
-    'data_db': 'bug-monitor',
-    'table': 'SecurityProblem'
-}
 
 CFG = Config()
